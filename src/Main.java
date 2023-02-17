bfa2006197206c8204d4884f07d4108e84e524c5
Moses <hungzenwai@gmail.com>
2023-02-17 03:00:46 +0000

    first video

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        //println needed for new line compared to print on the same line
         System.out.println("Hello world!");
+        System.out.println("Pizza");
+
+        //can also use \n
+        System.out.print("I like pizza");
+        System.out.print("It's good \n");
+
+        // \t adds a tab
+        System.out.println("\t this is a tab");
+        // to add quotes in we need a backslash
+        System.out.println("\" This is a quote \"");
+
+        /*
+        these are comments
+         */
+
+        //for System.Out.Println we can just type sout + tab
     }
 }
